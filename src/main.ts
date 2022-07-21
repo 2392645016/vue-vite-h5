59801287b8d76a307f968e5e166b844104b05cac
longyadong <202780181@qq.com>
2022-07-21 12:30:26 +0800

    feat:add Pinia

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import { Button } from 'vant';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
+import { createPinia } from 'pinia'
 
 import './styles/index.scss';
 
@@ -12,5 +13,6 @@ dayjs.locale('zh-ch');
 app.config.globalProperties.$dayjs = dayjs;
 
 app.use(Button);
+app.use(createPinia());
 
 app.mount('#app');
